e53369e7dcc1d9fa0c508dbcc802fa7898a58d98
mhmdho <hadiomidm@gmail.com>
2023-09-24 01:04:26 +0330

    Get a sample data for each unique type and layer of entities
    Signed-off-by: mhmdho <hadiomidm@gmail.com>

--- 0_info.py
+++ 0_info.py
@@ -18,3 +18,14 @@ for entity in msp:
     # myset.add(entity.dxftype())
     myset.add(entity.dxf.layer)
 print(myset)
+
+
+# Get a sample data for each dfxtype/layer
+mylist = list(myset)
+for entity in msp:
+    # thefilter = entity.dxftype()
+    thefilter = entity.dxf.layer
+    if thefilter in mylist:
+        print(thefilter)
+        print(entity.dxf.all_existing_dxf_attribs())
+        mylist.remove(thefilter)
