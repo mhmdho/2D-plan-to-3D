a26fd7072c3f4e59eb034e30d5cf4caed109f8ef
mhmdho <hadiomidm@gmail.com>
2023-11-23 12:31:22 +0330

    Code preprocessing of new algorithm

---
+++ b1_clustering_preProcessing.py
@@ -0,0 +1,49 @@
+import ezdxf
+from utils import transform_point, radius_scale
+
+
+doc = ezdxf.readfile('inputDXF/1.dxf')
+msp = doc.modelspace()
+
+
+doc_new = ezdxf.new()
+msp_new = doc_new.modelspace()
+
+
+for entity in msp:
+    if entity.dxftype() == 'INSERT':
+        layer = entity.dxf.layer
+        block = entity.doc.blocks[entity.dxf.name]
+        for e in block:
+            if e.dxf.invisible == 0:
+                if e.dxftype() == 'LINE':
+                    start = transform_point(e.dxf.start, entity)
+                    end = transform_point(e.dxf.end, entity)
+                    msp_new.add_line(start=start, end=end, dxfattribs={"layer": layer})
+                elif e.dxftype() in ['LWPOLYLINE', 'POLYLINE']:
+                    try:                     
+                        points = [transform_point(vertex, entity) for vertex in e.vertices()]
+                        msp_new.add_lwpolyline(points, dxfattribs={"layer": layer})
+                    except:
+                        pass
+                elif e.dxftype() == 'CIRCLE':
+                    center = transform_point(e.dxf.center, entity)
+                    radius_x=e.dxf.radius*entity.dxf.xscale
+                    radius_y=e.dxf.radius*entity.dxf.yscale
+                    msp_new.add_circle(center=center, radius=max(radius_y, radius_x), dxfattribs={"layer": layer})
+                elif e.dxftype() == 'ARC':
+                    center = transform_point(e.dxf.center, entity)
+                    radius_x=e.dxf.radius*entity.dxf.xscale
+                    radius_y=e.dxf.radius*entity.dxf.yscale
+                    msp_new.add_arc(center=center, radius=radius_scale(e, entity), 
+                                    start_angle=e.dxf.start_angle+entity.dxf.rotation, 
+                                    end_angle=e.dxf.start_angle+entity.dxf.rotation,
+                                    dxfattribs={"layer": layer})
+    elif entity.dxftype() not in ['MTEXT', 'TEXT', 'DIMENSION', 'WIPEOUT']:
+        try:
+            msp_new.add_foreign_entity(entity)
+        except:
+            pass
+
+
+doc_new.saveas('inputDXF/1.dxf')
