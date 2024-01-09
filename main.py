8d9cac8385059ddc1c1aa01d5061cbfcba1b3028
songhori <m.songhori@gmail.com>
2024-01-09 05:16:45 +0330

    Added a comment to main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import a_DWG2DXF
 import b1_clustering_preProcessing
 import b2_DXF_decomposition_newAlgorithm
 import b3_translationOfPlans
+
+# Below lines should be applied after renaming the elevation and plan decomposed files:
 import b4_matchingPlans
 import c0_layers_preProcessing
 import c_3DModel
