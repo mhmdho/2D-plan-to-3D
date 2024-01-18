63c078b2bdc42748393b2a04cf4cddeb4bc86c37
mhmdho <hadiomidm@gmail.com>
2024-01-18 20:26:31 +0330

    Specify to get only dxf files for converting
    Signed-off-by: mhmdho <hadiomidm@gmail.com>

--- DXFconvertor.py
+++ DXFconvertor.py
@@ -5,15 +5,16 @@ import os
 path = "decomposed/"
 
 for file in os.listdir(path):
-    cadImage = cad.Image.load(f"{path}{file}")
+    if file.split('.')[1] == 'dxf':
+        cadImage = cad.Image.load(f"{path}{file}")
 
-    rasterizationOptions = cad.imageoptions.CadRasterizationOptions()
+        rasterizationOptions = cad.imageoptions.CadRasterizationOptions()
 
-    pdfOptions = cad.imageoptions.PdfOptions()
-    cadImage.save(f"{path}{file.split('.')[0]}.pdf", pdfOptions)
+        pdfOptions = cad.imageoptions.PdfOptions()
+        cadImage.save(f"{path}{file.split('.')[0]}.pdf", pdfOptions)
 
-    # pngOptions = cad.imageoptions.PngOptions()
-    # cadImage.save("1.png", pngOptions)
+        # pngOptions = cad.imageoptions.PngOptions()
+        # cadImage.save("f"{path}{file.split('.')[0]}.png", pngOptions)
 
-    # dxfOptions = cad.imageoptions.DxfOptions()
-    # cadImage.save("1.dxf", dxfOptions)
+        # dxfOptions = cad.imageoptions.DxfOptions()
+        # cadImage.save("f"{path}{file.split('.')[0]}.dxf", dxfOptions)
