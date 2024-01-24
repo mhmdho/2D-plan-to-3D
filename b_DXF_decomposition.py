9b6b1106823efe819c3c4c2004e6ad33360d1d59
mhmdho <hadiomidm@gmail.com>
2024-01-25 01:05:36 +0330

    Add more details, other clustering algorithm and clean code
    Signed-off-by: mhmdho <hadiomidm@gmail.com>

--- b_DXF_decomposition.py
+++ b_DXF_decomposition.py
@@ -1,5 +1,6 @@
-from utils import transform_point, radius_scale, rotate_arc
-from sklearn.cluster import KMeans
+from utils import transform_point, radius_scale, rotate_arc, oghlidosi
+from sklearn.cluster import KMeans, DBSCAN, AgglomerativeClustering
+import numpy as np
 import ezdxf
 
 
@@ -30,6 +31,34 @@ def recreate_entity_in_msp(entity, msp):
 doc = ezdxf.readfile('inputDXF/1.dxf')
 msp = doc.modelspace()
 
+
+#---------------get all lines for clustering-------------
+'''
+# List of all layer names
+layer_names = [layer.dxf.name for layer in doc.layers]
+# layer_names = ["FP-Proposed Wall"]
+# print(layer_names)
+
+all_lines = []
+for layer_name in layer_names:
+    msp = doc.modelspace()
+    entities_on_layer = msp.query(f'LINE[layer=="{layer_name}"]')
+    new_doc = ezdxf.new()
+    msp = new_doc.modelspace()
+    for line in entities_on_layer:
+        start_point = line.dxf.start
+        end_point = line.dxf.end
+        all_lines.append((start_point, end_point))
+
+lines_array = np.array(all_lines)
+
+# Reshape the array to a single dimension
+lines_array = lines_array.reshape((-1, 6))
+'''
+#------------------------------------------------------
+
+
+#---------------get centers for clustering-------------
 centroids = []
 entities_with_centroids = []
 
@@ -55,9 +84,12 @@ for entity in msp:
     if centroid:
         centroids.append(centroid)
         entities_with_centroids.append(entity)
+#------------------------------------------------------
 
 
-# Number of expected plans/elevations
+#------------------------KMeans------------------------
+# num_clusters = 8  # Replace with the desired number of clusters
+# Number of expected plans/elevations (automating num_clusters)
 wcss = []
 max_clusters = 20  # Maximum number of clusters to try
 for n_clusters in range(1, max_clusters + 1):
@@ -68,17 +100,97 @@ for n_clusters in range(1, max_clusters + 1):
         num_clusters = n_clusters
         break
 
-
 kmeans = KMeans(n_clusters=n_clusters)
 labels = kmeans.fit_predict(centroids)
 
+# Get cluster labels for each line
+cluster_labels = kmeans.labels_
 
-for i in range(n_clusters):
+# . . . . Plot the WCSS values . . . .
+# print(wcss)
+# plt.plot(range(1, max_clusters + 1), wcss)
+# plt.xlabel('Number of Clusters')
+# plt.ylabel('WCSS')
+# plt.title('Elbow Method')
+# plt.show()
+#------------------------------------------------------
+
+
+#------------------------Oghlidosi---------------------
+# all_dist = []
+# new_doc = ezdxf.new()
+# msp = new_doc.modelspace()
+# for i in range(len(all_lines)):
+#     msp.add_line(start=all_lines[i][0], end=all_lines[i][1])
+#     if i > 2:
+#         d1 = oghlidosi(all_lines[i-2][-1], all_lines[i-3][-1])
+#         d2 = oghlidosi(all_lines[i-1][-2], all_lines[i-2][-1])
+
+#         if d2 > 6200:
+#             # oghlidosi(all_lines[-1][-2], all_lines[-2][-1]) > 20*oghlidosi(all_lines[-2][-1], all_lines[-3][-1]): 
+#             all_dist.append(d1)
+#             # i = len(all_dist)
+#             new_doc.saveas(f"seplayer/file{i}.dxf")
+#             new_doc = ezdxf.new()
+#             msp = new_doc.modelspace()
+
+# print(len(all_dist))
+#------------------------------------------------------
+
+
+#------------------------DBSCAN------------------------
+'''
+dbscan = DBSCAN(eps=0.00000000001, min_samples=2)
+# Fit the model to your data
+# Get the cluster labels for each point
+cluster_labels = dbscan.fit_predict(lines_array)
+
+print(*cluster_labels)
+print(len(cluster_labels))
+
+# Separate lines into different groups based on cluster labels
+num_clusters = len(set(cluster_labels)) - (1 if -1 in cluster_labels else 0)
+'''
+#------------------------------------------------------
+
+
+#-----------------AgglomerativeClustering--------------
+'''
+# Apply Agglomerative Clustering
+clustering = AgglomerativeClustering(n_clusters=8)
+cluster_labels = clustering.fit_predict(lines_array)
+
+# Determine the number of clusters
+num_clusters = len(np.unique(cluster_labels))
+'''
+#------------------------------------------------------
+
+
+'''
+# Separate lines into different groups based on cluster labels
+clusters = [[] for _ in range(num_clusters)]
+for i, label in enumerate(cluster_labels):
+    # if label != -1: # for dbscan
+        clusters[label].append(all_lines[i])
+'''
+
+for i in range(num_clusters):
+# for i, cluster in enumerate(clusters):
+    # print(f"Cluster {i+1}:")
     new_doc = ezdxf.new()
     msp_new = new_doc.modelspace()
-    
-    # Entities corresponding to current cluster
+    # for line in cluster:
+    #     msp.add_line(start=line[0], end=line[1])
     entities_for_cluster = [entity for j, entity in enumerate(entities_with_centroids) if labels[j] == i]
+    # for entity in entities_for_cluster:
+    #     recreate_entity_in_msp(entity, msp_new)
+    # new_doc.saveas(f"decomposed/cluster_{i}.dxf")
+    # or
+    # from ezdxf.addons import Importer
+    # importer = Importer(doc, new_doc)
+    # importer.import_entity(entity, msp_new)
+
+
     entities_layers = list({str(entity.dxf.layer).lower() for j, entity in enumerate(entities_with_centroids) if labels[j] == i})
     txt_layer = ' '.join(entities_layers)
     if len(entities_for_cluster) < 100:
@@ -91,7 +203,7 @@ for i in range(n_clusters):
         filename = f'plan_{i}'
     if 'foot' in txt_layer:
         filename = f'elevation_{i}'
-        
+
     for entity in entities_for_cluster:
         if entity.dxftype() in ['MTEXT']:
             if 'room' in str(entity.text).lower():
@@ -103,6 +215,9 @@ for i in range(n_clusters):
             for e in block:
                 if e.dxf.invisible == 0:
                     if e.dxftype() == 'LINE':
+                        # e.dxf.start = transform_point(e.dxf.start, entity, scale=False)
+                        # e.dxf.end = transform_point(e.dxf.end, entity, scale=False)
+                        # msp_new.add_foreign_entity(e)
                         start = transform_point(e.dxf.start, entity)
                         end = transform_point(e.dxf.end, entity)
                         msp_new.add_line(start=start, end=end, dxfattribs={"layer": layer})
